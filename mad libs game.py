f1ea4fd536fea74df8c2610207041b37f926e20c
Omkar Gharat <83211956+OmkarGharat@users.noreply.github.com>
2021-05-01 15:24:15 +0530

    Add files via upload

---
+++ mad libs game.py
@@ -0,0 +1,32 @@
+# Mad libs game
+'''
+Ask the user for an input.
+
+This could be anything such as a name, an adjective, a pronoun or even an action.
+Once you get the input, you can rearrange it to build up your own story.
+'''
+
+noun1 = input("Enter a boy's name : ")
+noun2 = input("Enter a girl's name : ")
+adjective1 = input("Enter an adjective1 :")
+adjective2 = input("Enter an adjective2 :")
+place = input("Enter a place : ")
+
+print('===========================================================================================================')
+print("Love Story : ")
+print("Once Upon a time , there was a boy named",noun1,".")
+print("He is very",adjective1,"student and a",adjective1,"friend as well.")
+print('----------------------------------------------------------------------------------------------------------')
+print("One day , he saw a girl named",noun2)
+print("As",noun1,"saw",noun2,",he started",adjective2,"her.")
+print("he follows",noun2,"on instagram")
+print("he save each and every photo of",noun2,"in his phone")
+print("he makes himself so capable that ...",noun1,"proposes to",noun2,"for marriage and",noun2,"can't reject him")
+print('----------------------------------------------------------------------------------------------------------')
+print("One day,",noun1,"proposes",noun2,"for marriage , with a diamond ring")
+print(noun2,"accepts his proposal and at",place,",",noun1,"married with",noun2)
+print('----------------------------------------------------------------------------------------------------------')
+print("And after 3 years , they had 2 kids")
+print("1st child is a girl who is like",noun2)
+print("2nd child is a boy who is like",noun1)
+print('===========================================================================================================')
